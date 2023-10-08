e559f15b91408f96fe0b975829dacc08b9a2a719
Andrés Felipe Bolaños <ghostafbr@gmail.com>
2023-10-08 02:09:12 -0500

    Update books.service.test.js

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,9 +1,28 @@
 const BooksService = require('./books.service');
 
+const fakeBooks = [
+    {
+        _id: 1,
+        title: 'Clean Code',
+    },
+    {
+        _id: 2,
+        title: 'The pragmatic programmer',
+    }
+];
+
+const MongoLibStub = {
+    getAll: () => [...fakeBooks],
+    create: () => {},
+}
+
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+
 describe('Tests for booksService', () => {
     let service;
     beforeEach(() => {
         service = new BooksService();
+        jest.clearAllMocks();
     });
 
     describe('test for getBooks', () => {
@@ -11,10 +30,18 @@ describe('Tests for booksService', () => {
            // Arrange
            // Act
            const books = await service.getBooks({});
-           console.log(books);
            // Assert
            expect(books.length).toEqual(2);
        });
+
+        test('should return an array of books', async () => {
+            // Arrange
+            // Act
+            const books = await service.getBooks({});
+            // Assert
+            expect(books[0].title).toEqual('Clean Code');
+        });
     });
+
 
 });
