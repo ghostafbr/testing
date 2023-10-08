1ac4a685d52b7ad6bb3f63e661c3e60307c857d2
Andrés Felipe Bolaños <ghostafbr@gmail.com>
2023-10-07 20:43:01 -0500

    Create 05-setup.test.js

---
+++ demos/src/05-setup.test.js
@@ -0,0 +1,41 @@
+describe('Set', () => {
+
+    beforeAll(() => {
+        console.log('beforeAll');
+        // up db
+    });
+
+    afterAll(() => {
+        console.log('afterAll');
+        // down db
+    });
+
+    beforeEach(() => {
+        console.log('beforeEach');
+        // up db
+    });
+
+    afterEach(() => {
+        console.log('afterEach');
+        // down db
+    });
+
+    test('case 1', () => {
+        expect(1 + 1).toBe(2);
+    });
+
+    test('case 2', () => {
+        expect(1 + 3).toBe(4);
+    });
+
+    describe('group 2', function () {
+        test('case 3', () => {
+            expect(1 + 1).toBe(2);
+        });
+
+        test('case 4', () => {
+            expect(1 + 3).toBe(4)
+        });
+
+    });
+});
