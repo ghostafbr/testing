b26aa8c7bedcb5476b9cde48f9a5f205f555a4b6
Andrés Felipe Bolaños <ghostafbr@gmail.com>
2023-10-07 22:42:46 -0500

    Update 06-person.test.js

--- demos/src/06-person.test.js
+++ demos/src/06-person.test.js
@@ -1,17 +1,21 @@
 const Person = require('./06-person');
 
-let person;
+describe('', () => {
+    let person;
+    // Arrange
+    beforeEach(() => {
+        person = new Person('Juan', 70, 1.70);
+    });
 
-beforeEach(() => {
-    const person = new Person('Juan', 70, 1.70);
-});
+    test('Should return down', () => {
+        // Arrange
+        person.weight = 45;
+        // Act
+        expect(person.calcIMC()).toBe('down');
+    });
 
-test('Should return down', () => {
-    person.weight = 45;
-    expect(person.calcIMC()).toBe('down');
-});
-
-test('Should return normal', () => {
-    person.weight = 56;
-    expect(person.calcIMC()).toBe('normal');
+    test('Should return normal', () => {
+        person.weight = 56;
+        expect(person.calcIMC()).toBe('normal');
+    });
 });
