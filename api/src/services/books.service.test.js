a64a7a2a81ab6b2f9e652fc2c357d19b89ba5e8c
Andrés Felipe Bolaños <ghostafbr@gmail.com>
2023-10-09 02:47:09 -0500

    Update books.service.test.js

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,39 +1,39 @@
 const BooksService = require('./books.service');
-const {generateManyBooks} = require("../../fakes/book.fake");
+const { generateManyBooks } = require('../../fakes/book.fake');
 
 const mockGetAll = jest.fn();
 
 jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
-    getAll: mockGetAll,
-    create: () => {},
+  getAll: mockGetAll,
+  create: () => {},
 })));
 
 describe('Test for BooksService', () => {
-    let service;
-    beforeEach(() => {
-        service = new BooksService();
-        jest.clearAllMocks();
+  let service;
+  beforeEach(() => {
+    service = new BooksService();
+    jest.clearAllMocks();
+  });
+
+  describe('tests for getBooks', () => {
+    test('should return a list book', async () => {
+      // Arrange
+      const fakeBooks = generateManyBooks(20);
+      mockGetAll.mockResolvedValue(fakeBooks);
+      // Act
+      const books = await service.getBooks({});
+      // Assert
+      expect(books.length).toEqual(fakeBooks.length);
+      expect(mockGetAll).toHaveBeenCalled();
+      expect(mockGetAll).toHaveBeenCalledTimes(1);
+      expect(mockGetAll).toHaveBeenCalledWith('books', {});
     });
 
-    describe('tests for getBooks', () => {
-        test('should return a list book', async () => {
-            // Arrange
-            const fakeBooks = generateManyBooks(20);
-            mockGetAll.mockResolvedValue(fakeBooks);
-            // Act
-            const books = await service.getBooks({});
-            // Assert
-            expect(books.length).toEqual(fakeBooks.length);
-            expect(mockGetAll).toHaveBeenCalled();
-            expect(mockGetAll).toHaveBeenCalledTimes(1);
-            expect(mockGetAll).toHaveBeenCalledWith('books', {});
-        });
-
-        test('should return a list book', async () => {
-            const fakeBooks = generateManyBooks(3);
-            mockGetAll.mockResolvedValue(fakeBooks);
-            const books = await service.getBooks({});
-            expect(books[0].name).toEqual(fakeBooks[0].name);
-        });
+    test('should return a list book', async () => {
+      const fakeBooks = generateManyBooks(3);
+      mockGetAll.mockResolvedValue(fakeBooks);
+      const books = await service.getBooks({});
+      expect(books[0].name).toEqual(fakeBooks[0].name);
     });
+  });
 });
